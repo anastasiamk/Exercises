f698da0d724ef1898663469942927acdb8dd7abb
anastasiamk <makarshina88@gmail.com>
2020-09-17 13:02:56 +0300

    ex3.2 first commit

---
+++ Ex3.2/ex3.2.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Упражнение 3.2 Создать структуру данных, которая хранит информацию
+ * о банковском счете – его номер, тип и баланс. Создать переменную такого типа,
+ * заполнить структуру значениями и напечатать результат.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3._2
+{
+    class Program
+    {
+        public enum BankType { current, savings }
+        public struct BankAccount
+        {
+            public double balance;
+            public string nomber;
+            public BankType banktype;
+        }
+        static void Main(string[] args)
+        {
+            BankAccount tinkoff;
+            tinkoff.balance = 2100;
+            tinkoff.nomber = "012536";
+            tinkoff.banktype = BankType.current ;
+            Console.WriteLine("Bank account nomber is: {0}, balanse = {1}, type: {2}", tinkoff.nomber, tinkoff.balance, tinkoff.banktype);
+        }
+    }
+}
