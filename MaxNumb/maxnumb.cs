5a7e8d6a8b8192fd952351a7f7e727b998bb4242
anastasiamk <makarshina88@gmail.com>
2020-09-17 16:32:40 +0300

    finding the maximum number

---
+++ MaxNumb/maxnumb.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Даны два числа, необходимо найти и вывести максимальное
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumb
+{
+    class maxnumb
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Max number:{0}", Math.Max(number1, number2));
+        }
+    }
+}
