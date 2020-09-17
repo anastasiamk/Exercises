3e91c02c3f832148f7481a878e1d0ba28f6560ab
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-17 20:44:33 +0300

    easy version

---
+++ NextData/nextdata.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Дана корректная дата в виде трех целых чисел. Получить дату следующего дня.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextData
+{
+    class nextdata
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current data: ");
+            DateTime time_1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(time_1);
+            Console.WriteLine(time_1.AddDays(1));
+        }
+    }
+}
