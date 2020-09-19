bf84355c3948842913c8ca3298b118054d358d37
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-19 21:42:36 +0300

    expr2 first commit

---
+++ Expr2/expr2.cs
@@ -0,0 +1,30 @@
+﻿/*Expr2. Задается натуральное трехзначное число 
+ * (гарантируется, что трехзначное). Развернуть его, т.е. получить 
+ * трехзначное число, записанное теми же цифрами в обратном порядке.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr1
+{
+    class expr2
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            char[] array = a.ToCharArray();
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+
+            
+            
+        }
+    }
+}
