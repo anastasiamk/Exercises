2fff0ec29a92d0434c0ab75f1bd01ef95544bfc8
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-19 15:41:27 +0300

    hw4.2a first commit

---
+++ Ex4.2a/ex4.2a.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 2.Задана точка своими двумя координатами. Принадлежит ли точка заштрихованной области
+ * a)
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4._2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (( (x * x) + (y*y -2*y + 1) <= 1 ) && (y + x*x <= 1) )
+            {
+                Console.WriteLine("Point belongs to the shaded area");
+            }
+            else
+            {
+                Console.WriteLine("Point does not belongs to the shaded area");
+            }
+        }
+    }
+}
