db2c36eaf021202b240e95e52cf932d033c663ba
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-19 22:24:08 +0300

    ulearn expr2

---
+++ Expr_2/expr_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr_2
+{
+    class expr_2
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            char[] array = a.ToCharArray();
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
