2c02a77c65f63e19261a2e2e8a4da1ec31e74e65
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-22 21:53:52 +0300

    ex6 first commit

---
+++ 6/6.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 6. Вычислить сумму 𝑦 = 1 + 𝑥 + (𝑥^2/2!) + (𝑥^3/3!) +...+ (𝑥^𝑛/𝑛!)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            int i = 1;
+            double f = 1;
+            double y = 1;
+
+            while(i <= n)
+            {
+                f *= i;
+                y += Math.Pow(x, i) / f;
+                i++;
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
