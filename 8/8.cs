91f6e0f8691994e8658e4c3932f910bb61843a1f
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-09-22 23:02:12 +0300

    ex8 first commit

---
+++ 8/8.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * 8. Вывести целые числа из отрезка [6;30], которые делятся на 3.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 6; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
