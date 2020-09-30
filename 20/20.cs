3c8d2f375e48c53dae8a0e09625e40dad89c84a7
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-01 00:12:45 +0300

    №20

---
+++ 20/20.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 20. Распечатать таблицу Пифагора (таблица умножения)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<= 9;i++)
+            {
+                for (int k = 1; k<= 9; k++)
+                {
+                    Console.Write("{0}\t ", (i*k));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
