d13fc4a71e9961b0666c8bc2f62807185d2e9eff
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-13 21:46:11 +0300

    №25

---
+++ 25/25.cs
@@ -0,0 +1,27 @@
+﻿//25. Написать метод для вычисления n
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25
+{
+    class Program
+    {
+        public static int Factorial(int a)
+        {
+            int res = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
