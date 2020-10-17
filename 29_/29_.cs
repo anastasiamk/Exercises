6a76570e5412432f5b6116722bf51134a5d194cf
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-17 20:45:55 +0300

    №29

---
+++ 29_/29_.cs
@@ -0,0 +1,25 @@
+﻿//29. Написать метод, который увеличивает передаваемое в метод число на единицу.
+// Параметр передавать по ссылке.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_
+{
+    class Program
+    {
+        public static void Add1(ref int k)
+        {
+            k += 1;
+        }
+        static void Main(string[] args)
+        {
+            int s = 25;
+            Add1(ref s);
+            Console.WriteLine(s);
+        }
+    }
+}
