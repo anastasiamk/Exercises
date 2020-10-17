89f3ed6a8d95d02ee4cfa8bfbaf241d02c1f2c9b
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-17 20:45:12 +0300

    №28

---
+++ 28_/28_.cs
@@ -0,0 +1,35 @@
+﻿//28. Написать метод, который натуральное число переворачивает в обратном порядке
+//(число 1234 превращается в 4321). Методу параметр передавать по ссылке. 
+//В результате работы метода должно меняться само исходное число.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_
+{
+    class Program
+    {
+        public static void TurnOver1(ref int n)
+        {
+            string w = "";
+
+            while(n > 0)
+            {
+                w += n % 10;
+                n /= 10;
+            }
+            n = int.Parse(w);
+
+
+        }
+        static void Main(string[] args)
+        {
+            int a = 23598;
+            TurnOver1(out a);
+            Console.WriteLine(a);
+        }
+    }
+}
