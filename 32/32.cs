91735095108e73cf7f32793ce8a55dc116458f00
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-20 22:12:44 +0300

    №32

---
+++ 32/32.cs
@@ -0,0 +1,30 @@
+﻿//32. Написать рекурсивный метод, который находит сумму цифр числа.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32
+{
+    class Program
+    {
+        public static int Summa(int k)
+        {
+            if (k < 10)
+            {
+                return k;
+            }
+            else
+            {
+                return (k%10) + Summa(k/10);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Summa(355));
+        }
+    }
+}
