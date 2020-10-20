1ddf8704aa3595d1ee23f1fd4814a69d98c69f69
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-20 22:12:03 +0300

    №31

---
+++ 31/31.cs
@@ -0,0 +1,29 @@
+﻿//31. Написать рекурсивный метод для вычисления n-го числа ряда Фибоначчи
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31
+{
+    class Program
+    {
+        public static int Fibonacci(int x)
+        {
+            if ((x == 0) || (x == 1))
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(x - 1) + Fibonacci(x - 2);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(5));
+        }
+    }
+}
