232b66441197a7447200b8e419b5b7e0b977e7a1
Anastasia Makarshina <makarshinaa1@gmail.com>
2020-10-20 22:13:35 +0300

    №33

---
+++ 33/33.cs
@@ -0,0 +1,33 @@
+﻿//33. Написать рекурсивный метод возведения числа в целую неотрицательную степень.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33
+{
+    class Program
+    {
+        public static int Power(int n, int k)
+        {
+            if (k == 0)
+            {
+                return 1;
+            }
+            else if(k == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Power(n, (k-1));
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Power(6, 0));
+        }
+    }
+}
